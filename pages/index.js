0c779a52c8dfd5f781a8f15ff0df7249258f9569
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-12-17 11:10:19 +0100

    FEAT AppContext

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Budget from "../components/Budget";
 import Remaining from "../components/Remaining";
 import Spent from "../components/Spent";
 import ExpenseList from "../components/ExpenseList";
+import AddExpenseForm from "../components/AddExpenseForm";
 const Home = () => {
   return (
     <div className="container">
@@ -23,6 +24,12 @@ const Home = () => {
       <div className="row mt-3">
         <div className="col-sm">
           <ExpenseList />
+        </div>
+      </div>
+      <h3 className="mt-3">Add Expense</h3>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <AddExpenseForm />
         </div>
       </div>
     </div>
